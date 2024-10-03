6155b3808affb54235418effc25b6f2d863666b3
Everlyse Tay <shihching.tay@dxc.com>
2024-10-03 16:14:49 +0800

    Adding output folder to git

---
+++ output/nem12.sql
@@ -0,0 +1,2 @@
+INSERT INTO meter_readings (nmi, timestamp, consumption) VALUES (NEM1201009, '20050301', '0.461');
+INSERT INTO meter_readings (nmi, timestamp, consumption) VALUES (NEM1201010, '20050301', '0.154');
